e2fa62999c45d9647251d96ce4b13abf259dd917
Jatjarin <theworm.raper@gmail.com>
2026-08-15 13:02:58 +0700

    chore: prepare Google Drive watcher deploy config

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: ["./src/__tests__/setup.ts"],
+    exclude: ["**/node_modules/**", "**/.next/**", "**/.worktrees/**", "**/.superpowers/**"],
   },
   resolve: {
     alias: {
